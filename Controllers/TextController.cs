3608c2f8d366bd384cc2fc3e884b95fe82fa49dd
vroyibg <vroybg@gmail.com>
2017-12-06 18:05:17 +0700

    Add db context to text controller

--- Controllers/TextController.cs
+++ Controllers/TextController.cs
@@ -1,10 +1,17 @@
 using System;
+using LWT.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LWT.Controllers
 {
     public class TextController:Controller
     {
+        private readonly LWTContext _context;
+
+        public TextController(LWTContext context)
+        {
+            _context = context;
+        }
         // The index page show list of all text in current language
         public IActionResult Index()
         {
