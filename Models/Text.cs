9dd3f88ee4f6d355d831d5fbcfe9a47566300992
vroyibg <vroybg@gmail.com>
2017-12-06 17:22:12 +0700

    Update required fields

--- Models/Text.cs
+++ Models/Text.cs
@@ -1,17 +1,21 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace LWT.Models
 {
     public class Text
     {
         public int ID { get; set; }
+        [Required]
         public string Content { get; set; } 
 
+        [Required]
         public string Title { get; set; }
         public string AudioURL { get; set; }
         public string SourceURL { get; set; }
 
+        [Required]
         public Language Language { get; set; }
     }
 }
