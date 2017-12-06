e855c06aae774ce62dd1d0f98a92e4f5c2da8589
vroyibg <vroybg@gmail.com>
2017-12-06 17:42:16 +0700

    Create TextController

---
+++ Controllers/TextController.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LWT.Controllers
+{
+    public class TextController:Controller
+    {
+        // The index page show list of all text in current language
+        public string Index()
+        {
+            // Todo: Implement
+            return "This is text page";
+        }
+    }
+}
