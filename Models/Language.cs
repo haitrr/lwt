a27399229566677d1030a083e89e7ca9724e3ec2
vroyibg <vroybg@gmail.com>
2017-12-06 17:29:51 +0700

    Update Language's require fields

--- Models/Language.cs
+++ Models/Language.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace LWT.Models
 {
     public class Language
     {
         public int ID { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public int TextSize { get; set; }
 
     }
