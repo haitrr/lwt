bd55f904e6ac2f899d778a621ee9f7030b5e83c6
vroyibg <vroybg@gmail.com>
2017-12-06 17:59:39 +0700

    Add view model for list of texts

---
+++ Models/ListTextViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace LWT.Models
+{
+    public class ListTextViewModel
+    {
+        public List<Text> Texts { get; set; }
+        
+    }
+}
