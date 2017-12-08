a26c7e983933c7eeb49b346600be918bbd0255d9
vroyibg <vroyibg@gmail.com>
2017-12-08 17:00:25 +0700

    Add text service interface

---
+++ Services/ITextService.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using LWT.Models;
+
+
+namespace LWT.Services
+{
+    public interface ITextService
+    {
+        bool IsExist(int id);
+        void Add(Text text);
+        void Remove(Text text);
+        Text GetByID(int id);
+        void Update(Text text);
+        List<Text> GetAll();
+
+    }
+}
