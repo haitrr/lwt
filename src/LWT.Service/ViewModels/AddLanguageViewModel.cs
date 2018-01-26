731b788b6849a3b22e9e4a0c42b8d5250bf364d6
vroyibg <vroyibg@gmail.com>
2018-01-26 18:45:19 +0700

    Add view model to add language

---
+++ src/LWT.Service/ViewModels/AddLanguageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LWT.Service.ViewModels
+{
+    public class AddLanguageViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
