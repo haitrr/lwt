3d802b6685d64df200aa7867459439677e293e25
vroyibg <vroyibg@gmail.com>
2018-01-26 18:59:15 +0700

    Add add method

--- src/LWT.Service/Interfaces/ILanguageService.cs
+++ src/LWT.Service/Interfaces/ILanguageService.cs
@@ -1,8 +1,10 @@
+using LWT.Service.ViewModels;
+using System.Threading.Tasks;
 
 namespace LWT.Service.Interfaces
 {
     public interface ILanguageService : IService
     {
-
+        public Task<bool> Add(AddLanguageViewModel addLanguageViewModel);
     }
 }
