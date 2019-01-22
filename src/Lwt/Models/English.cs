efc4f20bdbe4c41566b0caa613a97ff45ad0ff52
vroyibg <vroyibg@gmail.com>
2019-01-23 00:05:07 +0700

    fix english

--- src/Lwt/Models/English.cs
+++ src/Lwt/Models/English.cs
@@ -18,13 +18,13 @@ namespace Lwt.Models
         /// <inheritdoc />
         public bool ShouldSkip(string term)
         {
-            return Regex.IsMatch(term, @"([^a-zA-Z0-9\’\'])");
+            return Regex.IsMatch(term, @"([^a-zA-Z\’\'])");
         }
 
         /// <inheritdoc/>
         public string[] SplitText(string text)
         {
-            return Regex.Split(text, @"([^a-zA-Z0-9\’\'])")
+            return Regex.Split(text, @"([^a-zA-Z\’\'])")
                 .Where(word => !string.IsNullOrEmpty(word))
                 .ToArray();
         }
