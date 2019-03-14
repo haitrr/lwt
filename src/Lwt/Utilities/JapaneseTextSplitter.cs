98383243678e568b784c9e247225231ea3af8478
vroyibg <vroyibg@gmail.com>
2019-03-14 22:26:11 +0700

    fix japanese tokenizer

--- src/Lwt/Utilities/JapaneseTextSplitter.cs
+++ src/Lwt/Utilities/JapaneseTextSplitter.cs
@@ -1,6 +1,5 @@
 namespace Lwt.Utilities
 {
-    using System;
     using System.Collections.Generic;
     using System.IO;
     using Lucene.Net.Analysis.Ja;
@@ -10,66 +9,28 @@ namespace Lwt.Utilities
     /// <summary>
     /// Japanese text splitter.
     /// </summary>
-    public class JapaneseTextSplitter : IJapaneseTextSplitter, System.IDisposable
+    public class JapaneseTextSplitter : IJapaneseTextSplitter
     {
-        private readonly JapaneseTokenizer segmenter;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="JapaneseTextSplitter"/> class.
-        /// </summary>
-        public JapaneseTextSplitter()
-        {
-            this.segmenter = new JapaneseTokenizer(null, null, false, JapaneseTokenizerMode.NORMAL);
-        }
-
-        /// <summary>
-        /// Finalizes an instance of the <see cref="JapaneseTextSplitter"/> class.
-        /// </summary>
-        ~JapaneseTextSplitter()
-        {
-            this.Dispose(false);
-        }
-
         /// <inheritdoc />
         public IEnumerable<string> Split(string text)
         {
-            this.segmenter.SetReader(new StringReader(text));
-            this.segmenter.Reset();
             var terms = new List<string>();
 
-            while (this.segmenter.IncrementToken())
+            using (var reader = new StringReader(text))
             {
-                var term = this.segmenter.GetAttribute<ICharTermAttribute>();
-                terms.Add(term.ToString());
+                using (var tokenizer = new JapaneseTokenizer(reader, null, false, JapaneseTokenizerMode.NORMAL))
+                {
+                    tokenizer.Reset();
+
+                    while (tokenizer.IncrementToken())
+                    {
+                        var term = tokenizer.GetAttribute<ICharTermAttribute>();
+                        terms.Add(term.ToString());
+                    }
+                }
             }
 
             return terms;
-        }
-
-        /// <inheritdoc />
-        public void Dispose()
-        {
-            this.Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        /// <summary>
-        /// dispose resource.
-        /// </summary>
-        /// <param name="disposing">disposing.</param>
-        protected virtual void Dispose(bool disposing)
-        {
-            this.ReleaseUnmanagedResources();
-
-            if (disposing)
-            {
-                this.segmenter?.Dispose();
-            }
-        }
-
-        private void ReleaseUnmanagedResources()
-        {
-            // TODO release unmanaged resources here
         }
     }
 }
