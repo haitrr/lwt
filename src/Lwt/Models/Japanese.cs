405869f7dc90a2e8162a042354477136a69d1000
vroyibg <vroyibg@gmail.com>
2019-03-16 19:25:22 +0700

    japanese skip punction

--- src/Lwt/Models/Japanese.cs
+++ src/Lwt/Models/Japanese.cs
@@ -37,7 +37,7 @@ namespace Lwt.Models
         {
             return !Regex.IsMatch(
                 term,
-                @"^[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]+$");
+                @"^[\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]+$");
         }
 
         /// <inheritdoc />
