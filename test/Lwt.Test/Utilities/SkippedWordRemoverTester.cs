897252d301c48e2971ffb3e1e3daea2f572a9079
vroyibg <vroyibg@gmail.com>
2019-10-14 21:37:11 +0700

    test skipped words remover

--- test/Lwt.Test/Utilities/SkippedWordRemoverTester.cs
+++ test/Lwt.Test/Utilities/SkippedWordRemoverTester.cs
@@ -1,6 +1,10 @@
 namespace Lwt.Test.Utilities
 {
+    using System.Collections.Generic;
+    using Lwt.Interfaces;
+    using Lwt.Models;
     using Lwt.Utilities;
+    using Moq;
     using Xunit;
 
     /// <summary>
@@ -8,6 +12,18 @@ namespace Lwt.Test.Utilities
     /// </summary>
     public class SkippedWordRemoverTester
     {
+        private readonly SkippedWordRemover skippedWordRemover;
+        private readonly Mock<ILanguageHelper> languageHelperMock;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SkippedWordRemoverTester"/> class.
+        /// </summary>
+        public SkippedWordRemoverTester()
+        {
+            this.languageHelperMock = new Mock<ILanguageHelper>();
+            this.skippedWordRemover = new SkippedWordRemover(this.languageHelperMock.Object);
+        }
+
         /// <summary>
         /// should get resolved by DI.
         /// </summary>
@@ -16,6 +32,25 @@ namespace Lwt.Test.Utilities
         {
             var helper = new DependencyResolverHelper();
             Assert.IsType<SkippedWordRemover>(helper.GetService<ISkippedWordRemover>());
+        }
+
+        /// <summary>
+        /// test if the remover removes the right words.
+        /// </summary>
+        [Fact]
+        public void SkippedWordRemoverShouldRemoveSkippedWord()
+        {
+            var words = new List<string> { "sdf", "dsfsd", "dfjk", "sdf" };
+            var languageMock = new Mock<ILanguage>();
+            languageMock.Setup(l => l.ShouldSkip("sdf")).Returns(true);
+            this.languageHelperMock.Setup(h => h.GetLanguage(It.IsAny<Language>()))
+                .Returns(languageMock.Object);
+
+            IEnumerable<string> result = this.skippedWordRemover.RemoveSkippedWords(words, Language.English);
+
+            Assert.DoesNotContain("sdf", result);
+            Assert.Contains("dsfsd", result);
+            Assert.Contains("dfjk", result);
         }
     }
 }
