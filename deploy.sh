171861f501a1521154ca343b008ad613d6fabf3e
vroyibg <vroyibg@gmail.com>
2020-07-19 12:55:26 +0700

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+if [[ $1 == '' ]]
+then
+    echo 'Please input provide password to replace'
+    exit 0
+fi
+
+echo 'replace password'
+cd ./src/Lwt/Utilities/
+sed -i -e "s/\"q\"/\"$1\"/g" ./DatabaseSeeder.cs
+
+cd ..
+
+echo 'stop service'
+sudo systemctl stop lwt-api.service
+
+echo 'build app'
+dotnet build -c freevn -o publish
+
+echo 'start service'
+sudo systemctl start lwt-api.service
+
+echo 'service status'
+sudo systemctl status lwt-api.service
