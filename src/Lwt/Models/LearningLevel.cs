69d67f0192f2efcc52cf3b93f3587a9319e77cae
vroyibg <vroyibg@gmail.com>
2020-08-23 00:37:59 +0700

    add learning level type safe enum

---
+++ src/Lwt/Models/LearningLevel.cs
@@ -0,0 +1,110 @@
+namespace Lwt.Models
+{
+    using System;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// language code.
+    /// </summary>
+    [JsonConverter(typeof(LearningLevelJsonConverter))]
+    public sealed class LearningLevel
+    {
+        /// <summary>
+        /// Skipped.
+        /// </summary>
+        public static readonly LearningLevel Skipped = new LearningLevel(LearningLevelValue.Skipped);
+
+        /// <summary>
+        /// Ignored.
+        /// </summary>
+        public static readonly LearningLevel Ignored = new LearningLevel(LearningLevelValue.Ignored);
+
+        /// <summary>
+        /// Unknown.
+        /// </summary>
+        public static readonly LearningLevel Unknown = new LearningLevel(LearningLevelValue.Unknow);
+
+        /// <summary>
+        /// Learning 1.
+        /// </summary>
+        public static readonly LearningLevel Learning1 = new LearningLevel(LearningLevelValue.Learning1);
+
+        /// <summary>
+        /// Learning 2.
+        /// </summary>
+        public static readonly LearningLevel Learning2 = new LearningLevel(LearningLevelValue.Learning2);
+
+        /// <summary>
+        /// Learning 3.
+        /// </summary>
+        public static readonly LearningLevel Learning3 = new LearningLevel(LearningLevelValue.Learning3);
+
+        /// <summary>
+        /// Learning 4.
+        /// </summary>
+        public static readonly LearningLevel Learning4 = new LearningLevel(LearningLevelValue.Learning4);
+
+        /// <summary>
+        /// Learning 5.
+        /// </summary>
+        public static readonly LearningLevel Learning5 = new LearningLevel(LearningLevelValue.Learning5);
+
+        /// <summary>
+        /// Learning1.
+        /// </summary>
+        public static readonly LearningLevel WellKnown = new LearningLevel(LearningLevelValue.WellKnown);
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LearningLevel"/> class.
+        /// </summary>
+        /// <param name="value"> value.</param>
+        private LearningLevel(string value)
+        {
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Gets or sets value of code.
+        /// </summary>
+        public string Value { get; protected set; }
+
+        /// <summary>
+        /// get language code from string.
+        /// </summary>
+        /// <param name="code">code.</param>
+        /// <returns>language code.</returns>
+        /// <exception cref="NotSupportedException">language code is not supported.</exception>
+        public static LearningLevel GetFromString(string code)
+        {
+            switch (code)
+            {
+                case LearningLevelValue.Skipped:
+                    return Skipped;
+                case LearningLevelValue.Unknow:
+                    return Unknown;
+                case LearningLevelValue.Ignored:
+                    return Ignored;
+                case LearningLevelValue.Learning1:
+                    return Learning1;
+                case LearningLevelValue.Learning2:
+                    return Learning2;
+                case LearningLevelValue.Learning3:
+                    return Learning3;
+                case LearningLevelValue.Learning4:
+                    return Learning4;
+                case LearningLevelValue.Learning5:
+                    return Learning5;
+                case LearningLevelValue.WellKnown:
+                    return WellKnown;
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return this.Value;
+        }
+    }
+}
