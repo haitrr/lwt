19a4192076fe373cd08834c1ed5b578e680a44e6
vroyibg <vroyibg@gmail.com>
2020-08-28 00:00:08 +0700

    remove redundant check in text term mapper

--- src/Lwt/Mappers/TextTermMapper.cs
+++ src/Lwt/Mappers/TextTermMapper.cs
@@ -1,27 +1,16 @@
 namespace Lwt.Mappers
 {
-    using Lwt.Interfaces;
     using Lwt.Models;
     using Lwt.Services;
 
     public class TextTermMapper : BaseMapper<TextTerm, TermReadModel>
     {
-        private readonly ILanguageHelper languageHelper;
-
-        public TextTermMapper(ILanguageHelper languageHelper)
-        {
-            this.languageHelper = languageHelper;
-        }
-
         public override TermReadModel Map(TextTerm from, TermReadModel result)
         {
-            ILanguage language = this.languageHelper.GetLanguage(from.Text.LanguageCode);
             result.Content = from.Content;
             result.Id = from.Term?.Id;
             result.Index = from.Index;
-            result.LearningLevel = from.Term == null
-                ? language.ShouldSkip(from.Content) ? LearningLevel.Skipped : LearningLevel.Unknown
-                : from.Term.LearningLevel;
+            result.LearningLevel = from.Term == null ? LearningLevel.Skipped : from.Term.LearningLevel;
             return result;
         }
     }
