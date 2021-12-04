630f51d73d3b234eaac37b364376ec5b900a90d2
Hai Tran <vroyibg@gmail.com>
2021-12-04 15:54:13 +0700

    fix wrong nameoff

--- src/Lwt/Services/TextReader.cs
+++ src/Lwt/Services/TextReader.cs
@@ -59,7 +59,7 @@ public class TextReader : ITextReader
                 Constants.TextReadEvent,
                 new TextReadLogData(text.Id, text.LanguageCode.ToString()),
             authenticationHelper.GetLoggedInUserName()
-        ).WithEntity(nameof(text),text.Id)
+        ).WithEntity(nameof(Text),text.Id)
             );
         await this.dbTransaction.CommitAsync();
         return this.textReadMapper.Map(text);
