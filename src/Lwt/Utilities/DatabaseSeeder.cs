b8dee7167c5b1b6daa3d6c8bec4dc7dfdc0bca64
Hai Tran <vroyibg@gmail.com>
2021-12-08 23:56:34 +0700

    seed only user

--- src/Lwt/Utilities/DatabaseSeeder.cs
+++ src/Lwt/Utilities/DatabaseSeeder.cs
@@ -1,17 +1,12 @@
 namespace Lwt.Utilities;
 
-using System;
-using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using Lwt.DbContexts;
 using Lwt.Interfaces;
-using Lwt.Interfaces.Services;
 using Lwt.Models;
 using Lwt.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json.Linq;
 
 /// <inheritdoc />
 public class DatabaseSeeder : IDatabaseSeeder
@@ -19,8 +14,6 @@ public class DatabaseSeeder : IDatabaseSeeder
     private readonly IUserRepository userRepository;
 
     private readonly IdentityDbContext lwtDbContext;
-    private readonly ITextService textService;
-    private readonly ITermService termService;
     private readonly ISqlTextRepository textRepository;
     private readonly ILogger<DatabaseSeeder> logger;
 
@@ -29,22 +22,16 @@ public class DatabaseSeeder : IDatabaseSeeder
     /// </summary>
     /// <param name="userRepository"> user repo.</param>
     /// <param name="lwtDbContext">the db context.</param>
-    /// <param name="textService">the text service.</param>
-    /// <param name="termService">the term service.</param>
     /// <param name="textRepository">the text repository.</param>
     /// <param name="logger">the logger.</param>
     public DatabaseSeeder(
         IUserRepository userRepository,
         IdentityDbContext lwtDbContext,
-        ITextService textService,
-        ITermService termService,
         ISqlTextRepository textRepository,
         ILogger<DatabaseSeeder> logger)
     {
         this.userRepository = userRepository;
         this.lwtDbContext = lwtDbContext;
-        this.textService = textService;
-        this.termService = termService;
         this.textRepository = textRepository;
         this.logger = logger;
     }
